f2fb21e53abb7f43dde3670ba15fd64c1c3e269b
Matthias Z. <matthias.zimmermann@htlstp.at>
2024-11-12 20:40:27 +0100

    Added Cover image

---
+++ src/components/CoverImage.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+    <img class="cover-image" src="../assets/images/f0x-home-top-cover.webp">
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import CoverImage from '../components/CoverImage.vue';
 </script>
 
 <template>
+  <CoverImage />
 </template>
